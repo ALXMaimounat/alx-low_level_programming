1a7334b3c711fb27678e4e5b33e097e3e61a5f63
ALXMaimounat <maioued113@gmail.com>
2022-03-15 00:13:59 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *
- *
+ *main - prints the phrase with proper grammar,
+ *but the outcom is a piece of art
+ *Return: 
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of arts");
-	return (0)
+	printf("with proper grammar, but the outcome is a piece of arts,\n");
+	return (0);
 }
