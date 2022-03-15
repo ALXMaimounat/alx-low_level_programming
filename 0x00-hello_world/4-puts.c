6e82df117588660bad18268474915a907861cbd2
ALXMaimounat <maioued113@gmail.com>
2022-03-15 00:23:35 +0000

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - prints the phrase "programmi,g is like building"
- * Return: 0 if existed properly
+ * Return: 0 if existed properly, non-zero otherwise
  * */
 
 
 int main(void)
 {
 	puts("\"Programming is like building a multiliangual puzzle");
-	return (0);
+		return (0);
 }
